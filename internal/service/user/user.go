9b93d9a9b2e103d86ac2abf449b6c544347038c1
Sergei Galichev <sergey.galichev@outlook.com>
2024-02-16 15:45:27 +1000

    added new custom errors; added check IsUserExists

--- internal/service/user/user.go
+++ internal/service/user/user.go
@@ -7,26 +7,39 @@ import (
 	"os"
 )
 
+var (
+	errNotValidEmailFormat    = errors.New("service: not valid email format")
+	errPassAndConfirmNotMatch = errors.New("service: password and confirm password does not match")
+	errAdminPassNotMatch      = errors.New("service: admin password not valid")
+	errUserExist              = errors.New("service: user already exists. not created")
+	errInvalidCredentials     = errors.New("service: invalid credentials")
+)
+
 func (s *service) CreateUser(createDTO *dto.UserCreateDTO) (int64, error) {
 	if !dto.IsValidEmail(createDTO.Email) {
-		return -1, errors.New("service: not valid email format")
+		return -1, errNotValidEmailFormat
 	}
 	if !dto.ArePasswordsMatch(createDTO.Password, createDTO.ConfirmPassword) {
-		return -1, errors.New("service: password and confirm password does not match")
+		return -1, errPassAndConfirmNotMatch
 	}
 
 	adminPass := os.Getenv("ADMIN_KEY")
 
 	if dto.IsAdminRole(createDTO.Role) && !dto.IsAdminPasswordValid(adminPass, createDTO.AdminKey) {
-		return -1, errors.New("service: admin password not valid")
+		return -1, errAdminPassNotMatch
 	}
 
-	d, err := converter.CreateDTOToUserDAO(createDTO)
+	createDAO, err := converter.ConvertCreateDTOToUserDAO(createDTO)
 	if err != nil {
 		return -1, err
 	}
 
-	return s.userRepository.CreateUser(d)
+	ok, err := s.userRepository.IsUserExists(createDAO.Email)
+	if ok {
+		return -1, errUserExist
+	}
+
+	return s.userRepository.CreateUser(createDAO)
 }
 
 func (s *service) LoginUser(loginDTO *dto.UserLoginDTO) (accessToken, refreshToken string, err error) {
@@ -36,7 +49,7 @@ func (s *service) LoginUser(loginDTO *dto.UserLoginDTO) (accessToken, refreshTok
 	}
 
 	if !dto.ComparePassAndHash(loginDTO.Password, d.PassHash) {
-		return "", "", errors.New("service: invalid credentials")
+		return "", "", errInvalidCredentials
 	}
 
 	accessToken, err = s.jwtHelper.GenerateAccessToken(d)
